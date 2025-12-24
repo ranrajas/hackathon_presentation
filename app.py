2e91000c2a94a5ed961721b018a0d5f155393ac9
ranrajas <39897919+ranrajas@users.noreply.github.com>
2025-12-24 16:55:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,40 +1,51 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
 from create_ppt import create_ppt
+from r2_upload import upload_to_r2
+import os
 
 app = Flask(__name__)
 
-@app.route("/health", methods=["GET"])
+@app.route("/health")
 def health():
     return {"status": "ok"}
 
 @app.route("/create-ppt", methods=["POST"])
 def create_ppt_api():
-    data = request.json
-    data = {
-        "title": "Cloud Migration Strategy",
-        "slides": [
-            {
-            "title": "Agenda",
-            "points": [
-                "What is Cloud Migration",
-                "Benefits",
-                "Challenges",
-                "Strategy"
-            ]
-            },
-            {
-            "title": "Migration Approaches",
-            "points": [
-                "Rehost (Lift & Shift)",
-                "Refactor",
-                "Replatform",
-                "Retire"
-            ]
-            }
-        ]
-    }
-    ppt_file = create_ppt(data)
-    return send_file(ppt_file, as_attachment=True)
+    if not request.is_json:
+        return {"error": "JSON body required"}, 400
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    data = request.get_json()
+    data = {
+      "title": "Cloud Migration Strategy",
+      "slides": [
+        {
+          "title": "Agenda",
+          "points": [
+            "What is Cloud Migration",
+            "Benefits",
+            "Challenges",
+            "Strategy"
+          ]
+        },
+        {
+          "title": "Migration Approaches",
+          "points": [
+            "Rehost (Lift & Shift)",
+            "Refactor",
+            "Replatform",
+            "Retire"
+          ]
+        }
+      ]
+    }
+
+
+    ppt_file = create_ppt(data)
+    object_key = f"presentations/{os.path.basename(ppt_file)}"
+
+    download_url = upload_to_r2(ppt_file, object_key)
+
+    return jsonify({
+        "status": "success",
+        "ppt_url": download_url
+    })
